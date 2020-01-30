e5c3c58adeb3ec6b56464abff72ec319a5090815
[Vitor Hugo] <silvalimavitorhugo@gmail.com>
2020-01-30 18:33:48 -0500

    Creating address model and installing rest framework

--- persons/models.py
+++ persons/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Address(models.Model):
+    address_line_1 = models.CharField(max_length=125)
+    address_line_2 = models.CharField(max_length=125)
+    address_line_3 = models.CharField(max_length=125)
+    country = models.CharField(max_length=50)
+    state = models.CharField(max_length=60)
+    city = models.CharField(max_length=60)
+    postal_code = models.CharField(max_length=16)
--- servicefusion/settings.py
+++ servicefusion/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'persons',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
