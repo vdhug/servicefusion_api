c07edd6ead57b053f764dc005573a6e51a0a70a8
[Vitor Hugo] <silvalimavitorhugo@gmail.com>
2020-02-07 11:31:05 -0500

    Put to update list of addresses and phones

--- persons/serializers.py
+++ persons/serializers.py
@@ -65,11 +65,40 @@ class PersonSerializer(serializers.ModelSerializer):
                 e.address = email['address']
                 e.save()
             except Email.DoesNotExist:
-                Email(address=email['address'], person=instance).save()
+                Email(**email, person=instance).save()
         
 
         phones = validated_data.pop('phones')
+        phones_to_delete = [phone['id'] for phone in phones]
+        # Delete phones that are not present in the object
+        instance.phones.all().exclude(id__in=phones_to_delete).delete()
+
+        for phone in phones:
+            try:
+                p = Phone.objects.get(id=phone['id'])
+                p.number = phone['number']
+                p.save()
+            except Phone.DoesNotExist:
+                Phone(**phone, person=instance).save()
+        
         addresses = validated_data.pop('addresses')
+        addresses_to_delete = [address['id'] for address in addresses]
+        # Delete addresses that are not present in the object
+        instance.addresses.all().exclude(id__in=addresses_to_delete).delete()
+
+        for address in addresses:
+            try:
+                a = Address.objects.get(id=address['id'])
+                a.address_line_1 = address['address_line_1']
+                a.address_line_2 = address['address_line_2']
+                a.address_line_3 = address['address_line_3']
+                a.country = address['country']
+                a.state = address['state']
+                a.city = address['city']
+                a.postal_code = address['postal_code']
+                a.save()
+            except Address.DoesNotExist:
+                Address(**address, person=instance).save()
 
         instance.first_name = validated_data.get('first_name', instance.first_name)
         instance.last_name = validated_data.get('last_name', instance.last_name)
